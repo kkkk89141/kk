4ee7e80766645a336a6a4353069863ff137aa42d
dtk14052007 <dtk14052007@gmail.com>
2025-02-07 23:31:50 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+let slideIndex = 0;
+
+function showSlides() {
+    let slides = document.getElementsByClassName("slide");
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) { slideIndex = 1 }
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlides, 5000); // Tự động chuyển ảnh sau 5 giây
+}
+
+function prevSlide() {
+    let slides = document.getElementsByClassName("slide");
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex--;
+    if (slideIndex < 1) { slideIndex = slides.length }
+    slides[slideIndex - 1].style.display = "block";
+}
+
+function nextSlide() {
+    let slides = document.getElementsByClassName("slide");
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) { slideIndex = 1 }
+    slides[slideIndex - 1].style.display = "block";
+}
+
+showSlides(); // Bắt đầu hiển thị ảnh
